4dc5645dbf04e371a9286f1842925fbbd971c0ad
Andy Sipe <ajs.general@gmail.com>
2012-10-10 07:12:39 -0400

    initial project setup

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Nucs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Z Bar Technologies, LLC")]
+[assembly: AssemblyProduct("Lenz")]
+[assembly: AssemblyCopyright("(c) 2012 Andy Sipe (ajs.general@gmail.com), Morgan Sipe (morgan.talk@gmail.com)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ec0c9892-7059-4021-9d3a-0e40e16214f1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
