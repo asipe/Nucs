8746c47321aa67ed7a29f49cbcf2c4c3a5bb3186
Andy Sipe <ajs.general@gmail.com>
2012-10-10 07:14:11 -0400

    clean up

--- src/Nucs.Console/Program.cs
+++ src/Nucs.Console/Program.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Nucs.Console {
-  class Program {
-    static void Main(string[] args) {
-    }
+﻿namespace Nucs.Console {
+  internal class Program {
+    private static void Main(string[] args) {}
   }
 }
