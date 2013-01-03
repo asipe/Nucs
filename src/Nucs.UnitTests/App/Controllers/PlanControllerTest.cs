c825d377f2ddbf3e8689a75529290d150edcbe3a
Andy Sipe <ajs.general@gmail.com>
2013-01-03 08:25:34 -0500

    added missing TC

---
+++ src/Nucs.UnitTests/App/Controllers/PlanControllerTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using Nucs.App.Controllers;
+using SupaCharge.Testing;
+
+namespace Nucs.UnitTests.App.Controllers {
+  [TestFixture]
+  public class PlanControllerTest : BaseTestCase {
+    [Test]
+    public void TestGetAllGivesEmptyArray() {
+      Assert.That(new PlanController().GetAll(), Is.Empty);
+    }
+  }
+}
