12b83a22c825f88471406f5180f193d20943b1b4
Andy Sipe <ajs.general@gmail.com>
2013-01-03 08:10:16 -0500

    added server startup script

---
+++ scripts/run_server.ps1
@@ -0,0 +1,6 @@
+﻿$path = Split-Path (Split-Path $MyInvocation.MyCommand.Path)
+$path = Join-Path $path 'debug\apps\Nucs.Console\nucs.exe'
+Write-Host -ForegroundColor Green "Starting $path"
+$process = new-object System.Diagnostics.ProcessStartInfo $path
+$process.Verb = 'runas'
+[System.Diagnostics.Process]::Start($process)
