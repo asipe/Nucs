8c017fb5e7a73f245836649bcaa92c5894cc10e3
Andy Sipe <ajs.general@gmail.com>
2013-03-10 11:38:38 -0400

    cleaned up script so it no longer runs as administrator

--- scripts/run_server.ps1
+++ scripts/run_server.ps1
@@ -1,6 +1,1 @@
-﻿$path = Split-Path (Split-Path $MyInvocation.MyCommand.Path)
-$path = Join-Path $path 'debug\apps\Nucs.Console\nucs.exe'
-Write-Host -ForegroundColor Green "Starting $path"
-$process = new-object System.Diagnostics.ProcessStartInfo $path
-$process.Verb = 'runas'
-[System.Diagnostics.Process]::Start($process)
+﻿.\debug\apps\nucs.console\nucs.exe
